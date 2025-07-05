c5b65b65753df1e913376490426f726203afc208
Yash Kagawake <kagawaleyash@gmail.com>
2025-07-05 18:39:44 +0530

    Second Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,11 +53,11 @@ const Navbar = ({ navOpen }) => {
       link: '#work',
       className: 'nav-link'
     },
-    {
-      label: 'Reviews',
-      link: '#reviews',
-      className: 'nav-link'
-    },
+    // {
+    //   label: 'Reviews',
+    //   link: '#reviews',
+    //   className: 'nav-link'
+    // },
     {
       label: 'Contact',
       link: '#contact',
